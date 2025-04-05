5fd1bf14e7d45d85f7fff190a3308c2d7f81421c
standen <tkadensky@ya.ru>
2025-04-05 18:43:52 +0300

    Added default colors; configured alias

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./assets/css/normalize.css";
-import { MyRouter } from "./router";
+import { MyRouter } from "@/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
