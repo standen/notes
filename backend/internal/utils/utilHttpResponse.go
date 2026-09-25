d4cd72f707df6140f69e56509095630a64595844
Nekrasov/D <tkadensky@Yandex.ru>
2026-09-25 15:27:27 +0300

    Update utilHttpResponse.go

--- backend/internal/utils/utilHttpResponse.go
+++ backend/internal/utils/utilHttpResponse.go
@@ -2,42 +2,45 @@ package utils
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"backend/internal/model"
 )
 
 func HttpResponse[T any](w http.ResponseWriter, statusCode int, res *model.Response[T]) {
+	body, err := json.Marshal(res)
+	if err != nil {
+		log.Printf("http: failed to encode response: %v", err)
+
+		statusCode = http.StatusInternalServerError
+		body, _ = json.Marshal(&model.Response[struct{}]{
+			Message: "Ошибка сериализации ответа",
+			Error:   err.Error(),
+		})
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-
-	if err := json.NewEncoder(w).Encode(res); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(`{"error":"Ошибка сериализации ответа","message":"Ошибка сериализации ответа"}`))
-	}
+	_, _ = w.Write(body)
 }
 
 func HttpSuccessWithData[T any](w http.ResponseWriter, statusCode int, result *T) {
-	res := model.Response[T]{
-		Result: result,
-	}
-	HttpResponse(w, statusCode, &res)
+	HttpResponse(w, statusCode, &model.Response[T]{Result: result})
 }
 
-func HttpSuccessNoData(w http.ResponseWriter, msg string) {
-	res := model.Response[struct{}]{
-		Message: msg,
-	}
-	HttpResponse(w, http.StatusOK, &res)
+func HttpSuccessNoData(w http.ResponseWriter, statusCode int, msg string) {
+	HttpResponse(w, statusCode, &model.Response[struct{}]{Message: msg})
 }
 
-func HttpError(w http.ResponseWriter, statusCode int, msg string, errArgs ...string) {
-	res := model.Response[struct{}]{
-		Message: msg,
-	}
-	
-	if len(errArgs) > 0 {
-		res.Error = errArgs[0]
-	}
-	HttpResponse(w, statusCode, &res)
+func HttpError(w http.ResponseWriter, statusCode int, msg string) {
+	writeError(w, statusCode, "", msg)
+}
+
+func HttpErrorWithCode(w http.ResponseWriter, statusCode int, code, msg string) {
+	writeError(w, statusCode, code, msg)
+}
+
+func writeError(w http.ResponseWriter, statusCode int, code, msg string) {
+	HttpResponse(w, statusCode, &model.Response[struct{}]{Error: code, Message: msg})
 }
